f7aa428d8b6f413662192a9226ced24718b23b00
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-02-25 11:15:18 -0600

    fri-mor

--- work.js
+++ work.js
@@ -1,1 +1,14 @@
 console.log('work js running')
+
+// async function loadGithubEvents(event) {
+//     event.preventDefault();
+
+
+//     const response = await octokit.request('GET /users/{username}/events', {
+//         username: "bajcula"
+//     })
+//     const data = await response.json();
+//     console.log(data)
+// }
+
+// $("#githubapi").on('click', loadGithubEvents)
