e7f11a16d677c7a303af6b1ec24cae351fedbdc4
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-03-02 10:39:50 -0600

    wed mor

--- contact.js
+++ contact.js
@@ -1,3 +1,12 @@
 console.log("js running")
 
 
+const openModal = () => {
+    $("#message-modal").css('display', 'block')
+}
+
+$("#message").on('click', openModal)
+
+$("#close-modal").on('click', () => {
+    $("#message-modal").css('display', 'none')
+})
